dc5ab231d8d7f4a7d1021c110ed69940f5781e47
wzq031025 <135501522+wzq031025@users.noreply.github.com>
2023-06-10 23:54:04 +0800

    Update TreatmentCourse.java

--- src/main/java/group57/emrsystem/TreatmentCourse.java
+++ src/main/java/group57/emrsystem/TreatmentCourse.java
@@ -1,4 +1,23 @@
 package group57.emrsystem;
 
 public class TreatmentCourse {
+    private int start_date; 
+    private int end_date;
+    
+     public TreatmentCourse(int start_date, int end_date){
+         this.start_date = start_date;
+         this.end_date = end_date;
+     }
+     
+     public TreatmentCourse getTreatmentCourse(){
+         return this;
+     }
+     
+     public void modifystart_date(int start_date){
+         this.start_date = start_date;
+     }
+     
+     public void modifyend_date(int end_date){
+         this.end_date = end_date;
+     }
 }
