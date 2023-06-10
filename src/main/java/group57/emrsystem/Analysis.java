d5de152e0e49839633adbdd09f33e21dff0b92da
wzq031025 <135501522+wzq031025@users.noreply.github.com>
2023-06-11 00:24:33 +0800

    Update Analysis.java

--- src/main/java/group57/emrsystem/Analysis.java
+++ src/main/java/group57/emrsystem/Analysis.java
@@ -1,4 +1,29 @@
 package group57.emrsystem;
 
 public class Analysis {
+    private int date;
+    private String type_of_test;
+    private String result;
+    
+    public Analysis(int date, String type_of_test, String result){
+        this.date = date;
+        this.type_of_test = type_of_test;
+        this.result = result;
+    }
+    
+    public Analysis getAnalysis (){
+        return this;
+    }
+    
+    public void modifydate(int date){
+        this.date = date;
+    }
+    
+    public void modifytype_of_test(String type_of_test){
+        this.type_of_test = type_of_test;
+    }
+    
+    public void modiftresult(String result){
+        this.result = result;
+    }
 }
