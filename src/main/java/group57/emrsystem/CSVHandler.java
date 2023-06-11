dc424ba0e94251aeab65f4381f7dbde081001cbf
Ranchoel <116049751+Ranchoel@users.noreply.github.com>
2023-06-11 19:26:49 +0800

    Create CSVHandler.java

---
+++ src/main/java/group57/emrsystem/CSVHandler.java
@@ -0,0 +1,21 @@
+package group57.emrsystem;
+import java.io.*;
+import java.util.*;
+
+public class CSVHandler {
+    private String delimiter = ",";
+    private String newline = "\n";
+    private String header = "id,name,age"; // e.g for Student class data
+
+    // default constructor
+    public CSVHandler() {
+        
+    }
+
+    // You can also use a constructor to set the delimiter, header and newline
+    public CSVHandler(String header, String delimiter, String newline) {
+        this.header = header;
+        this.delimiter = delimiter;
+        this.newline = newline;
+    }
+}
