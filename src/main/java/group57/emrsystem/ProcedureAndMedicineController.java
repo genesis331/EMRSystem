ee2c5de25825420fdae52ae39f3eee57e14fb7a6
Ranchoel <116049751+Ranchoel@users.noreply.github.com>
2023-06-13 02:05:13 +0800

    Update ProcedureAndMedicineController.java

--- src/main/java/group57/emrsystem/ProcedureAndMedicineController.java
+++ src/main/java/group57/emrsystem/ProcedureAndMedicineController.java
@@ -1,6 +1,8 @@
 package group57.emrsystem;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ProcedureAndMedicineController {
     @FXML
@@ -11,4 +13,25 @@ public class ProcedureAndMedicineController {
 
     @FXML
     private Table tableuser;
+
+    @FXML
+    private TextField textdateprocedure;
+
+    @FXML
+    private TextField texttimeprocedure;
+
+    @FXML
+    private TextField textproceduretypeprocedure;
+
+    @FXML
+    private TextField textmedicationprocedure;
+
+    @FXML
+    private TextField textamountprocedure;
+
+    @FXML
+    private TextField textfrequencyprocedure;
+
+    @FXML
+    private Button savebuttonprocedure;
 }
