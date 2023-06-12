ab7666a3150feae594886c1697e23ad0afceecc4
Ranchoel <116049751+Ranchoel@users.noreply.github.com>
2023-06-13 01:50:11 +0800

    Update ProcedureAndMedicineController.java

--- src/main/java/group57/emrsystem/ProcedureAndMedicineController.java
+++ src/main/java/group57/emrsystem/ProcedureAndMedicineController.java
@@ -1,4 +1,14 @@
 package group57.emrsystem;
 
+import javafx.fxml.FXML;
+
 public class ProcedureAndMedicineController {
+    @FXML
+    private Button addbuttonadmin;
+
+    @FXML
+    private Table tableadmin;
+
+    @FXML
+    private Table tableuser;
 }
