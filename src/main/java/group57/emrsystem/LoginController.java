d92a95c20f8fc72f6afb774f4209e52a715f6e9c
SyncPss <juin10.jt@gmail.com>
2023-06-13 02:07:51 +0800

    update part2 of login.fxml, LoginController.java, patient-admin.fxml, patient-user.fxml, and PatientController.java

--- src/main/java/group57/emrsystem/LoginController.java
+++ src/main/java/group57/emrsystem/LoginController.java
@@ -1,4 +1,19 @@
 package group57.emrsystem;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button loginButton;
+
 }
--- src/main/java/group57/emrsystem/PatientController.java
+++ src/main/java/group57/emrsystem/PatientController.java
@@ -1,4 +1,47 @@
 package group57.emrsystem;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TableView;
+
 public class PatientController {
+    @FXML
+    private TextField nationalIDField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField ageField;
+    @FXML
+    private TextField genderField;
+    @FXML
+    private TextField contactNumberField;
+    @FXML
+    private TextArea addressArea;
+    @FXML
+    private Button viewMedicalHistoryButton;
+    @FXML
+    private Button viewTreatmentCourseButton;
+    @FXML
+    private Button viewAnalysisFormButton;
+    @FXML
+    private Button viewDiagnosisFormButton;
+    @FXML
+    private Button viewProcedureandMedicineFormButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button addRecordButton;
+    @FXML
+    private Button logoutButtonUser;
+    @FXML
+    private Button logoutButtonAdmin;
+    @FXML
+    private TableView displayTable;
+
 }
