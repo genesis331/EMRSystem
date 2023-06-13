0500a8ab6baa4a8aa685abe0fd42cffdcbf005ad
wzq031025 <wzq031025@gmail.com>
2023-06-14 03:44:33 +0800

    analysis and treatmentcourse controller ,done

--- src/main/java/group57/emrsystem/AnalysisController.java
+++ src/main/java/group57/emrsystem/AnalysisController.java
@@ -1,5 +1,29 @@
 package group57.emrsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AnalysisController {
-    // test
+    @FXML
+    private TextField date_textfield;
+
+    @FXML
+    private TextField type_of_test_textfield;
+
+    @FXML
+    private TextField result_textfield;
+
+    @FXML
+    private Button save_button;
+
+    @FXML
+    private TableView analysis_admin_table;
+
+    @FXML
+    private Button add_record_button;
+
+    @FXML
+    private TableView analysis_user_table;
 }
--- src/main/java/group57/emrsystem/TreatmentCourseController.java
+++ src/main/java/group57/emrsystem/TreatmentCourseController.java
@@ -1,4 +1,29 @@
 package group57.emrsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class TreatmentCourseController {
+    @FXML
+    private TextField treatment_textfield;
+
+    @FXML
+    private TextField start_date_textfield;
+
+    @FXML
+    private TextField end_date_textfield;
+
+    @FXML
+    private Button save_button;
+
+    @FXML
+    private TableView treatment_admin_table;
+
+    @FXML
+    private Button add_record_button;
+
+    @FXML
+    private TableView treatment_user_table;
 }
