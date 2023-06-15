fd62d6d574942f7ce837dd9a585ab85c152a16b8
Billy T <billytaslim26@gmail.com>
2023-06-15 12:59:06 +0800

    Hope this works -Billy

--- src/main/java/group57/emrsystem/DiagnosisController.java
+++ src/main/java/group57/emrsystem/DiagnosisController.java
@@ -1,4 +1,20 @@
 package group57.emrsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class DiagnosisController {
+    @FXML
+    private TextField DateTextField;
+    private TextField NameTextField;
+    private TextField DiagnosedSicknessTextField;
+    private TableView AdminTableView;
+    private TableView PrivateTableView;
+    private Button SaveButton;
+    private Button AdminAddRecordButton;
+
+
 }
+
--- src/main/java/group57/emrsystem/MedicalHistoryController.java
+++ src/main/java/group57/emrsystem/MedicalHistoryController.java
@@ -1,4 +1,21 @@
 package group57.emrsystem;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 public class MedicalHistoryController {
+    @FXML
+    private TextField DateTextField;
+    private TextField TimeTextField;
+    private TextField WardTextField;
+    private TextField TreatmentResultsTextField;
+    private TextField ObervationTextField;
+    private TextField MajorComplicationsTextField;
+    private TextField AttendingDoctorTextField;
+    private Button SaveButton;
+    private TableView UserTableView;
+    private TableView AdminTableView;
+    private Button AdminAddRecordButton;
+
+
 }
