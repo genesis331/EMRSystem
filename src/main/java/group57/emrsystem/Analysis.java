cf23a5823513f552c242abcacb2f52240f020faf
wzq031025 <wzq031025@gmail.com>
2023-06-18 00:46:36 +0800

    csv, add new id field

--- src/main/java/group57/emrsystem/Analysis.java
+++ src/main/java/group57/emrsystem/Analysis.java
@@ -1,11 +1,13 @@
 package group57.emrsystem;
 
 public class Analysis {
+    private String id;
     private String date;
     private String type_of_test;
     private String result;
     
-    public Analysis(String date, String type_of_test, String result){
+    public Analysis(String id,String date, String type_of_test, String result){
+        this.id = id;
         this.date = date;
         this.type_of_test = type_of_test;
         this.result = result;
@@ -13,6 +15,10 @@ public class Analysis {
     
     public Analysis getAnalysis (){
         return this;
+    }
+
+    public void modifyid(String id){
+        this.id = id;
     }
     
     public void modifydate(String date){
--- src/main/java/group57/emrsystem/TreatmentCourse.java
+++ src/main/java/group57/emrsystem/TreatmentCourse.java
@@ -1,10 +1,12 @@
 package group57.emrsystem;
 
 public class TreatmentCourse {
+    private String id;
     private String start_date; 
     private String end_date;
     
-     public TreatmentCourse(String start_date, String end_date){
+     public TreatmentCourse(String id,String start_date, String end_date){
+         this.id = id;
          this.start_date = start_date;
          this.end_date = end_date;
      }
@@ -12,7 +14,11 @@ public class TreatmentCourse {
      public TreatmentCourse getTreatmentCourse(){
          return this;
      }
-     
+
+     public void modifyid(String id){
+         this.id = id;
+     }
+
      public void modifystart_date(String start_date){
          this.start_date = start_date;
      }
