1794c9667734c847b45b20cb4822fc68d80d7d41
genesis331 <cheah3838@gmail.com>
2023-06-28 01:32:42 +0800

    better ux

--- src/main/java/group57/emrsystem/PatientController.java
+++ src/main/java/group57/emrsystem/PatientController.java
@@ -364,8 +364,10 @@ public class PatientController implements Initializable {
             patientTable.getColumns().add(colBtn);
             patientTable.setItems(list);
         } else {
+            boolean found = false;
             for (Patient patient : data) {
                 if (patient.getNationalID().equals(username)) {
+                    found = true;
                     nationalIDField.setText(patient.getNationalID());
                     nameField.setText(patient.getName());
                     ageField.setText(patient.getAge());
@@ -373,6 +375,14 @@ public class PatientController implements Initializable {
                     contactNumberField.setText(patient.getContactNo());
                     addressArea.setText(patient.getAddress());
                 }
+            }
+            if (!found) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setHeaderText(null);
+                alert.setContentText("Patient not found!");
+                alert.showAndWait();
+                stage.close();
             }
         }
     }
