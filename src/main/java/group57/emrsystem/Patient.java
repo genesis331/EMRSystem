f138a79752514a0919e8f77151999663485004e7
SyncPss <juin10.jt@gmail.com>
2023-06-27 14:04:34 +0800

    update Patient.java

--- src/main/java/group57/emrsystem/Patient.java
+++ src/main/java/group57/emrsystem/Patient.java
@@ -1,26 +1,48 @@
 package group57.emrsystem;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Patient {
     private String id;
-    private String name;
-    private int national_id;
-    private int age;
-    private String gender;
-    private String address;
-    private int contact_no;
+    private SimpleStringProperty name;
+    private SimpleStringProperty national_id;
+    private SimpleStringProperty age;
+    private SimpleStringProperty gender;
+    private SimpleStringProperty address;
+    private SimpleStringProperty contact_no;
 
-    public Patient(String id, String name, int national_id, int age, String gender, String address, int contact_no) {
+    public Patient(String id, String name, String national_id, String age, String gender, String address, String contact_no) {
         this.id = id;
-        this.name = name;
-        this.national_id = national_id;
-        this.age = age;
-        this.gender = gender;
-        this.address = address;
-        this.contact_no = contact_no;
+        this.name = new SimpleStringProperty(name);
+        this.national_id = new SimpleStringProperty(national_id);
+        this.age = new SimpleStringProperty(age);
+        this.gender = new SimpleStringProperty(gender);
+        this.address = new SimpleStringProperty(address);
+        this.contact_no = new SimpleStringProperty(contact_no);
     }
 
-    public Patient getPatient() {
-        return this;
+    public String getName() {
+        return name.get();
+    }
+
+    public String getNational_ID() {
+        return national_id.get();
+    }
+
+    public String getAge() {
+        return age.get();
+    }
+
+    public String getGender() {
+        return gender.get();
+    }
+
+    public String getAddress() {
+        return address.get();
+    }
+
+    public String getContact_No() {
+        return contact_no.get();
     }
 
     public void modifyID(String id) {
@@ -31,11 +53,11 @@ public class Patient {
         this.name = name;
     }
 
-    public void modifyNational_ID(int national_id) {
+    public void modifyNational_ID(String national_id) {
         this.national_id = national_id;
     }
 
-    public void modifyAge(int age) {
+    public void modifyAge(String age) {
         this.age = age;
     }
 
@@ -47,7 +69,7 @@ public class Patient {
         this.address = address;
     }
 
-    public void modifyContact_No(int contact_no) {
+    public void modifyContact_No(String contact_no) {
         this.contact_no = contact_no;
     }
 }
